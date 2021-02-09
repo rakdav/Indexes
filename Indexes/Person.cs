4a7f2d40aa8afcf93f57b872cacd2a47d9bb7b6b
John Doe <gogs@fake.local>
2021-02-09 15:12:11 +0200

    Добавьте файлы проекта.

---
+++ Indexes/Person.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Indexes
+{
+    class Person
+    {
+        public enum Status
+        {
+            ребенок,школьник,студент,работник, пенсионер
+        }
+        public enum Health
+        {
+            здоров, нездоров, покатит
+        }
+        private string fam="";
+        private int age;
+        private int salary;
+        private Status status = Status.работник;
+        private Health health = Health.здоров;
+        private const int Max_Children = 20;
+        private Person[] children = new Person[Max_Children];
+        private int count_children = 0;
+        public int Count_children
+        {
+            get { return count_children; }
+        }
+
+        public Person this[int i]
+        {
+            get
+            {
+                if (i >= 0 && i < count_children) return children[i];
+                else return children[0];
+            }
+            set
+            {
+                if(i==count_children&&i<Max_Children)
+                {
+                    children[i] = value;
+                    count_children++;
+                }
+            }
+        }
+        public string Fam
+        {
+            get { return fam; }
+            set { if (fam=="") fam = value; }
+        }
+        public Status GetStatus
+        {
+            get
+            {
+                return status;
+            }
+        }
+        public Health GetHealth
+        {
+            get
+            { return health;}
+        }
+
+        public int Age
+        {
+            set
+            {
+                age = value;
+                if (age < 7) status = Status.ребенок;
+                else if (age < 17) status = Status.школьник;
+                else if (age < 22) status = Status.студент;
+                else if (age < 65) status = Status.работник;
+                else status = Status.пенсионер;
+            }
+            get { return age; }
+        }
+
+        public int Salary
+        {
+            set { salary = value; }
+        }
+
+    }
+}
---
+++ Indexes/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Indexes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person();
+            person1.Fam = "Иванов Петр Ильич";
+            person1.Age = 27;
+            Person person2 = new Person();
+            person2.Fam = "Иванов Илья Петрович";
+            person2.Age = 3;
+            person1[person1.Count_children] = person2;
+            Person person3 = new Person();
+            person3.Fam = "Иванова Анна Петровна";
+            person3.Age = 1;
+            person1[person1.Count_children] = person3;
+            Print(person1);
+            Print(person1[0]);
+            Print(person1[1]);
+            Console.WriteLine(person1.Count_children);
+        }
+        static void Print(Person p)
+        {
+            Console.WriteLine("Фамилия:{0}, возраст:{1}, статус:{2}",
+                p.Fam,p.Age,p.GetStatus);
+        }
+    }
+}
